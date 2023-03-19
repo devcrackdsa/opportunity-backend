042593c053e12d9334028118ee7eb887fe793283
utsav82 <utsa.yadav.94@gmail.com>
2023-03-19 17:01:16 +0530

    Added delete and patch

--- controller/jobs.js
+++ controller/jobs.js
@@ -1,6 +1,7 @@
 const jobItem = require("../model/job.js");
 const path = require("path");
-exports.getitems = async (req, res) => {
+const mongoose = require("mongoose");
+exports.getAllItems = async (req, res) => {
   try {
     let items = await jobItem.find({});
 
@@ -14,6 +15,7 @@ exports.getitems = async (req, res) => {
   }
 };
 
+//not working
 exports.getbytag = async (req, res) => {
   try {
     let tag = req.query.tag.trim();
@@ -38,7 +40,7 @@ exports.saveitem = async (req, res) => {
       title: req.body.title,
       company: req.body.company,
       desc: req.body.desc,
-      image: path.join("/static/img/", `${req.file.filename}`),
+      image: path.join("/static/img/", `${req.file?.filename}`),
       lastdate: req.body.lastdate,
       stipend: req.body.stipend,
       exclusive: req.body.exclusive,
@@ -53,9 +55,56 @@ exports.saveitem = async (req, res) => {
 
     let item = new jobItem(obj);
     await item.save();
-    res.status(200).json("Saved");
+    res.status(200).json(item);
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.status(500).json(err);
+  }
+};
+
+exports.getitem = async (req, res) => {
+  const id = req.params.id;
+  console.log({ id });
+  const job = await jobItem.findById(id);
+  res.json(job);
+};
+
+exports.updateitem = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const doc = await jobItem.findOne({ _id: id });
+
+    doc.title = req.body.title ? req.body.title : doc.title;
+    doc.company = req.body.company ? req.body.company : doc.company;
+    doc.desc = req.body.desc ? req.body.desc : doc.desc;
+    if (req.file) {
+      doc.image = path.join("/static/img/", `${req.file?.filename}`);
+    }
+    doc.lastdate = req.body.lastdate ? req.body.lastdate : doc.lastdate;
+    doc.stipend = req.body.stipend ? req.body.stipend : doc.stipend;
+    doc.exclusive = req.body.exclusive ? req.body.exclusive : doc.exclusive;
+    doc.live = req.body.live ? req.body.v : doc.live;
+    doc.location = req.body.location ? req.body.location : doc.location;
+    doc.duration = req.body.duration ? req.body.duration : doc.duration;
+    doc.url = req.body.url ? req.body.url : doc.url;
+    doc.tags = req.body.tags ? req.body.tags?.split(" ") : doc.tags;
+    doc.skills = req.body.skills ? req.body.skills?.split(" ") : doc.skills;
+    doc.requirements = req.body.requirements ? req.body.requirements?.split(" ") : doc.requirements;
+    await doc.save();
+    res.status(201).json(doc);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+};
+
+exports.deleteitem = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const doc = await jobItem.findByIdAndDelete({ _id: id });
+    res.status(201).json(doc);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
   }
 };
--- routes/jobs.js
+++ routes/jobs.js
@@ -2,8 +2,11 @@ const { router, upload, auth } = require("../functions/required");
 const jobscontroller = require("../controller/jobs");
 
 router
-  .get("/", jobscontroller.getitems)
+  .get("/", jobscontroller.getAllItems)
+  .get("/:id", jobscontroller.getitem)
   .get("/tags", jobscontroller.getbytag)
-  .post("/", upload.single('file'),auth, jobscontroller.saveitem);
+  .post("/", upload.single("file"), auth, jobscontroller.saveitem)
+  .patch("/:id", upload.single("file"),auth,jobscontroller.updateitem)
+  .delete("/:id",auth, jobscontroller.deleteitem);
 
 module.exports = router;
